2bd50e20a544885aca477fd1e3e27d4a730c6fa2
EmilDieterle <101996780+EmilDieterle@users.noreply.github.com>
2022-05-08 03:27:50 +0200

    LO6

---
+++ Steckbrief/L06/script.js
@@ -0,0 +1,18 @@
+var populationEU = 447.01;
+var countryGER = "Deutschland";
+var populationGER = 83.16;
+var populationGERFuture = 82.67;
+console.log("Die Bevölkerung von " + countryGER + " ist im Jahre 2021 " + populationGER + " Millionen. Relativ zur Gesamtzahl der Bevölkerung in der EU ist die Bevölkerung in " + countryGER + " in diesem Jahr: " + populationGER * 100 / populationEU + "%. Bis zum Jahre 2050 wird sich die Bevölkerung um " + (populationGERFuture - populationGER) / populationGER * 100 + "% verändern und " + populationGERFuture + " Millionen betragen. ");
+var countryFRA = "Frankreich";
+var populationFRA = 67.44;
+var populationFRAFuture = 70.01;
+console.log("Die Bevölkerung von " + countryFRA + " ist im Jahre 2021 " + populationFRA + " Millionen. Relativ zur Gesamtzahl der Bevölkerung in der EU ist die Bevölkerung in " + countryFRA + " in diesem Jahr: " + populationFRA * 100 / populationEU + "%. Bis zum Jahre 2050 wird sich die Bevölkerung um " + (populationFRAFuture - populationFRA) / populationFRA * 100 + "% verändern und " + populationFRAFuture + " Millionen betragen. ");
+var countryITA = "Italien";
+var populationITA = 59.26;
+var populationITAFuture = 58.13;
+console.log("Die Bevölkerung von " + countryITA + " ist im Jahre 2021 " + populationITA + " Millionen. Relativ zur Gesamtzahl der Bevölkerung in der EU ist die Bevölkerung in " + countryITA + " in diesem Jahr: " + populationITA * 100 / populationEU + "%. Bis zum Jahre 2050 wird sich die Bevölkerung um " + (populationITAFuture - populationITA) / populationFRA * 100 + "% verändern und " + populationITAFuture + " Millionen betragen. ");
+var countrySPA = "Spanien";
+var populationSPA = 47.39;
+var populationSPAFuture = 49.35;
+console.log("Die Bevölkerung von " + countrySPA + " ist im Jahre 2021 " + populationSPA + " Millionen. Relativ zur Gesamtzahl der Bevölkerung in der EU ist die Bevölkerung in " + countrySPA + " in diesem Jahr: " + populationSPA * 100 / populationEU + "%. Bis zum Jahre 2050 wird sich die Bevölkerung um " + (populationSPAFuture - populationSPA) / populationSPA * 100 + "% verändern und " + populationSPAFuture + " Millionen betragen. ");
+//# sourceMappingURL=script.js.map
