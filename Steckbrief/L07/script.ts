a7963861f524c71586598849e12852ebb6735e32
EmilDieterle <101996780+EmilDieterle@users.noreply.github.com>
2022-05-16 01:07:19 +0200

    L07 Update

--- Steckbrief/L07/script.ts
+++ Steckbrief/L07/script.ts
@@ -56,51 +56,9 @@ namespace L07 {
             document.getElementById("headlineRelativePopulation").innerHTML = (populationSPA * 100 / populationEU).toFixed(2)  + "%";
             document.getElementById("headlineGrowthRate").innerHTML = ((populationSPAFuture - populationSPA) / populationSPA *100).toFixed(2) + "%";
             document.getElementById("headlinePopulation2050").innerHTML = populationSPAFuture + "Mio";
-            //document.getElementsByClassName("wrapper.active").;
+            document.querySelector("germany").setAttribute("class", "active");
             //document.getElementById("chart"). = (100*populationEU) / populationSPA;
         });
 
     });
-
-    //function totalPopulation2(country : string, population : number, populationFuture : number) {
-        //console.log(population + "Mio");
-        //};
-
-    //totalPopulation2("Deutschland", 83.16, 82.67);
-   //totalPopulation2("Frankreich", 67.44, 70.01);
-    //totalPopulation2("Italien", 59.26, 58.13);
-    //totalPopulation2("Spanien", 47.39, 49.35);
-
-    //function relativeEU2(country : string, population : number, populationFuture : number) {
-        //console.log(population * 100 / populationEU  + "%.");
-   //};
-
-    //relativeEU2("Deutschland", 83.16, 82.67);
-    //relativeEU2("Frankreich", 67.44, 70.01);
-    //relativeEU2("Italien", 59.26, 58.13);
-    //relativeEU2("Spanien", 47.39, 49.35);
-
-    //function growthRate2(country : string, population : number, populationFuture : number) {
-        //console.log((populationFuture - population) / population *100 + "%");
-    //};
-
-    //growthRate2("Deutschland", 83.16, 82.67);
-    //growthRate2("Frankreich", 67.44, 70.01);
-    //growthRate2("Italien", 59.26, 58.13);
-    //growthRate2("Spanien", 47.39, 49.35);
-
-    //function populationCountryFuture2(country : string, population : number, populationFuture : number) {
-        //console.log(populationFuture + "Mio");
-    //};
-
-    //populationCountryFuture2("Deutschland", 83.16, 82.67);
-    //populationCountryFuture2("Frankreich", 67.44, 70.01);
-    //populationCountryFuture2("Italien", 59.26, 58.13);
-    //populationCountryFuture2("Spanien", 47.39, 49.35);
-
-
-    //" Bis zum Jahre 2050 wird sich die Bevölkerung um " + (populationFuture - population) / population *100 + "% verändern."+ "Bevölkerung im Jahre 2050 " + populationFuture + "Mio"
-
-    //})
-
 }
