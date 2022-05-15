79b2625da6181f7ce3766aa1f58cc45d427bc297
EmilDieterle <101996780+EmilDieterle@users.noreply.github.com>
2022-05-15 17:10:28 +0200

    L07

---
+++ Steckbrief/L07/script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    function selectCountry() {
+    }
+});
+//# sourceMappingURL=script.js.map
---
+++ Steckbrief/L07/script.ts
@@ -0,0 +1,13 @@
+var country = "germany";
+var country = "france"
+var country = "italy";
+var country = "spain";
+
+
+window.addEventListener("load", function() {
+
+    function selectCountry(){
+        
+    }
+
+})
