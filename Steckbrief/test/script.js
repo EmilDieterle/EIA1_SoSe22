8797826597fcb15c3b7d750f66da71de4824fa67
EmilDieterle <101996780+EmilDieterle@users.noreply.github.com>
2022-05-22 15:26:49 +0200

    L08

--- Steckbrief/test/script.js
+++ Steckbrief/test/script.js
@@ -1,11 +1,9 @@
 window.addEventListener("load", function () {
-    function saySomething() {
-        alert("der Daumen");
-    }
-    document.querySelector("#button1").addEventListener("click", saySomething);
-    function buttonClicked() {
-        document.querySelector("#result").innerHTML = "1";
-    }
-    document.querySelector("#button2").addEventListener("click", buttonClicked);
+    var colors = ["rgb(1,88,224)", "#ff0088", "yellow", "rgba(0,0,100,0.5)"];
+    var activeIndex = 0;
+    document.querySelector("#button").addEventListener("click", function () {
+        document.querySelector("body").style.backgroundColor = colors[activeIndex];
+        activeIndex = activeIndex + 1;
+    });
 });
 //# sourceMappingURL=script.js.map
