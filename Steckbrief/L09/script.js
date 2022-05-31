dde9f062b73da20e0d01464433cce65bc47277c5
EmilDieterle <101996780+EmilDieterle@users.noreply.github.com>
2022-05-31 14:03:30 +0200

    L09

--- Steckbrief/L09/script.js
+++ Steckbrief/L09/script.js
@@ -15,14 +15,15 @@ window.addEventListener("load", function () {
     document.querySelector(".fa-play").addEventListener("click", playButton);
     function playButton() {
         var beat = [
-            new Audio("assets/L08_task_material_assets_DrumPad_kick.mp3"),
-            new Audio("assets/L08_task_material_assets_DrumPad_snare.mp3"),
-            new Audio("assets/L08_task_material_assets_DrumPad_hihat.mp3")
+        // "assets/L08_task_material_assets_DrumPad_kick.mp3", 
+        //  "assets/L08_task_material_assets_DrumPad_snare.mp3", 
+        //  "assets/L08_task_material_assets_DrumPad_hihat.mp3",
         ];
         var activeIndex = 0;
         var intervalID;
         intervalID = setInterval(function () {
-            beat[activeIndex].play();
+            var beatAktuell = beat[activeIndex];
+            // playSample(beatAktuell);
             activeIndex = activeIndex + 1;
             if (activeIndex == beat.length) {
                 activeIndex = 0;
@@ -30,25 +31,43 @@ window.addEventListener("load", function () {
         }, 500);
         function stop() {
             clearInterval(intervalID);
-            document.getElementById("#playButton").addEventListener("click", stop);
         }
+        document.getElementById("#playButton").addEventListener("click", stop);
     }
-    //document.getElementById("myDIV").style.display = "none";
-    //document.getElementById("myDIV").style.display = "block";
-}
-//function playButton(): void {
-//  var beat: HTMLAudioElement[] = [
-// "assets/L08_task_material_assets_DrumPad_kick.mp3",
-//  "assets/L08_task_material_assets_DrumPad_snare.mp3",
-// "assets/L08_task_material_assets_DrumPad_hihat.mp3",
-// ];
-// var activeIndex: number = 0;
-// setInterval(function(): void {
-// var beatAktuell = beat[activeIndex];
-//playSample(beatAktuell);
-//  activeIndex = activeIndex + 1;
-//  }, 500);
-//Math.random() // erzeugt eine (quasi) Zufallszahl 
-//Math.round()  eine einfache Art um eine Gleitkommazahl zu einer Ganzzahl zu runden
-);
+    document.querySelector(".fa-music").addEventListener("click", playRemix);
+    function playRemix() {
+        var beat = [
+            new Audio("assets/L08_task_material_assets_DrumPad_kick.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_snare.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_hihat.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_A.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_C.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_F.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_G.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_laugh-1.mp3"),
+            new Audio("assets/L08_task_material_assets_DrumPad_laugh-2.mp3")
+        ];
+        var activeIndex;
+        setInterval(function () {
+            beat[Math.round(Math.random() * 9)].play();
+            activeIndex = activeIndex + 1;
+        }, 500);
+        //document.getElementById("myDIV").style.display = "none";
+        //document.getElementById("myDIV").style.display = "block";
+    }
+    //function playButton(): void {
+    //  var beat: HTMLAudioElement[] = [
+    // "assets/L08_task_material_assets_DrumPad_kick.mp3",
+    //  "assets/L08_task_material_assets_DrumPad_snare.mp3",
+    // "assets/L08_task_material_assets_DrumPad_hihat.mp3",
+    // ];
+    // var activeIndex: number = 0;
+    // setInterval(function(): void {
+    // var beatAktuell = beat[activeIndex];
+    //playSample(beatAktuell);
+    //  activeIndex = activeIndex + 1;
+    //  }, 500);
+    //Math.random() // erzeugt eine (quasi) Zufallszahl 
+    //Math.round()  eine einfache Art um eine Gleitkommazahl zu einer Ganzzahl zu runden
+});
 //# sourceMappingURL=script.js.map
--- Steckbrief/L09/script.ts
+++ Steckbrief/L09/script.ts
@@ -26,28 +26,51 @@ window.addEventListener("load", function (): void {
     document.querySelector(".fa-play").addEventListener("click", playButton);
     function playButton(): void {
     var beat: HTMLAudioElement[] = [
-        new Audio("assets/L08_task_material_assets_DrumPad_kick.mp3"), 
-        new Audio("assets/L08_task_material_assets_DrumPad_snare.mp3"), 
-        new Audio("assets/L08_task_material_assets_DrumPad_hihat.mp3")
+       // "assets/L08_task_material_assets_DrumPad_kick.mp3", 
+      //  "assets/L08_task_material_assets_DrumPad_snare.mp3", 
+      //  "assets/L08_task_material_assets_DrumPad_hihat.mp3",
     ];
     var activeIndex: number = 0;
     var intervalID;
 
     intervalID = setInterval(function(): void {
-    beat[activeIndex].play();
+    var beatAktuell: HTMLAudioElement = beat[activeIndex];
+   // playSample(beatAktuell);
     activeIndex = activeIndex + 1;
     if (activeIndex == beat.length) {
         activeIndex = 0;
     }
  },                          500);
 
-
     function stop(): void {
-    clearInterval(intervalID);
-    document.getElementById("#playButton").addEventListener("click", stop);
+        clearInterval(intervalID);
 }
 
+    document.getElementById("#playButton").addEventListener("click", stop);
+
 }
+
+    document.querySelector(".fa-music").addEventListener("click", playRemix);
+    function playRemix(): void {
+    var beat: HTMLAudioElement[] = [
+        new Audio("assets/L08_task_material_assets_DrumPad_kick.mp3"), 
+        new Audio("assets/L08_task_material_assets_DrumPad_snare.mp3"), 
+        new Audio("assets/L08_task_material_assets_DrumPad_hihat.mp3"),
+        new Audio("assets/L08_task_material_assets_DrumPad_A.mp3"),
+        new Audio("assets/L08_task_material_assets_DrumPad_C.mp3"),
+        new Audio("assets/L08_task_material_assets_DrumPad_F.mp3"),
+        new Audio("assets/L08_task_material_assets_DrumPad_G.mp3"),
+        new Audio("assets/L08_task_material_assets_DrumPad_laugh-1.mp3"),
+        new Audio("assets/L08_task_material_assets_DrumPad_laugh-2.mp3")
+    ];
+    var activeIndex: number;
+
+    setInterval(function(): void {
+    beat[Math.round(Math.random() * 9)].play();
+    activeIndex = activeIndex + 1;
+ },             500);
+
+
 
     //document.getElementById("myDIV").style.display = "none";
     //document.getElementById("myDIV").style.display = "block";
@@ -70,4 +93,4 @@ window.addEventListener("load", function (): void {
   //Math.random() // erzeugt eine (quasi) Zufallszahl 
     //Math.round()  eine einfache Art um eine Gleitkommazahl zu einer Ganzzahl zu runden
 
-);
+} );
