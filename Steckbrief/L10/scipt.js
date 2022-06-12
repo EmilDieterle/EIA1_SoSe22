073d36be3b962fdd070bb8146b1b75320677a1d8
EmilDieterle <dieterleemil@gmail.com>
2022-06-12 21:28:38 +0200

    L10

--- Steckbrief/L10/scipt.js
+++ Steckbrief/L10/scipt.js
@@ -1,1 +1,12 @@
+document.querySelector("fa-regular fa-circle").addEventListener("click", kreisAnkreuzen);
+function kreisAnkreuzen() {
+    document.querySelector("fa-regular fa-circle").classList.add("hidden");
+    document.querySelector("fa-regular fa-circle-check").classList.remove("hidden");
+    console.log("hallo");
+}
+document.querySelector("fa-regular fa-circle-check").addEventListener("click", kreisNichtAnkreuzen);
+function kreisNichtAnkreuzen() {
+    document.querySelector("fa-regular fa-circle-check").classList.add("hidden");
+    document.querySelector("fa-regular fa-circle").classList.remove("hidden");
+}
 //# sourceMappingURL=scipt.js.map
---
+++ Steckbrief/L10/scipt.ts
@@ -0,0 +1,12 @@
+document.querySelector("fa-regular fa-circle").addEventListener("click", kreisAnkreuzen);
+function kreisAnkreuzen(): void {
+     document.querySelector("fa-regular fa-circle").classList.add("hidden");
+     document.querySelector("fa-regular fa-circle-check").classList.remove("hidden");
+     console.log("hallo");
+    }
+
+document.querySelector("fa-regular fa-circle-check").addEventListener("click", kreisNichtAnkreuzen);
+function kreisNichtAnkreuzen(): void {
+         document.querySelector("fa-regular fa-circle-check").classList.add("hidden");
+         document.querySelector("fa-regular fa-circle").classList.remove("hidden");
+        }
