405d808390ce88358e6a59205dd89957e40c9b6b
EmilDieterle <dieterleemil@gmail.com>
2022-06-15 01:37:06 +0200

    L10

--- Steckbrief/L10/script.js
+++ Steckbrief/L10/script.js
@@ -10,17 +10,28 @@ addTask.addEventListener("click", function () {
     list.innerText = inputField.value;
     listContainer.appendChild(list);
     inputField.value = "";
+    var circleChecked = document.createElement("i");
+    circleChecked.classList.add("hidden");
+    console.log(circleChecked);
     var circle = document.createElement("i");
     circle.classList.add("fa-regular", "fa-circle");
     circle.addEventListener("click", function () {
         circle.classList.remove("fa-regular", "fa-circle");
         circle.classList.add("fa-regular", "fa-circle-check");
-        if (circle.getAttribute("fa-circle-check")) {
-            circle.classList.remove("fa-regular", "fa-circle-check");
-            circle.classList.add("fa-regular", "fa-circle");
-            console.log("sds");
-        }
+        //circle.addEventListener("click", function() {
+        //circle.classList.remove("fa-regular", "fa-circle");
+        //circle.classList.add("fa-regular", "fa-circle-check");
+        //if (circle.getAttribute("fa-circle-check")) {
+        //circle.classList.remove("fa-regular", "fa-circle-check");
+        //circle.classList.add("fa-regular", "fa-circle");
+        //circleChecked.classList.remove("hidden");
+        //circleChecked.classList.add("fa-regular", "fa-circle");
+        //console.log("sds");
+        //}
     });
+    //circle.addEventListener("click", function() {
+    //circle.classList.remove("fa-regular", "fa-circle-check");
+    //circle.classList.add("fa-regular", "fa-circle");
     var listItem = document.createElement("div");
     listItem.classList.add("listStyle");
     listItem.appendChild(trash);
@@ -35,15 +46,15 @@ addTask.addEventListener("click", function () {
         document.getElementById("tasksInTotal").innerHTML = i + " tasks in total";
     });
 });
-document.querySelector("fa-regular fa-circle").addEventListener("click", kreisAnkreuzen);
-function kreisAnkreuzen() {
-    document.querySelector("fa-regular fa-circle").classList.add("hidden");
-    document.querySelector("fa-regular fa-circle-check").classList.remove("hidden");
-    console.log(kreisAnkreuzen);
-}
-document.querySelector("fa-regular fa-circle-check").addEventListener("click", kreisNichtAnkreuzen);
-function kreisNichtAnkreuzen() {
-    document.querySelector("fa-regular fa-circle-check").classList.add("hidden");
-    document.querySelector("fa-regular fa-circle").classList.remove("hidden");
-}
+//document.querySelector("fa-regular fa-circle").addEventListener("click", kreisAnkreuzen);
+//function kreisAnkreuzen(): void {
+//document.querySelector("fa-regular fa-circle").classList.add("hidden");
+//document.querySelector("fa-regular fa-circle-check").classList.remove("hidden");
+//console.log(kreisAnkreuzen);
+//}
+//document.querySelector("fa-regular fa-circle-check").addEventListener("click", kreisNichtAnkreuzen);
+//function kreisNichtAnkreuzen(): void {
+//document.querySelector("fa-regular fa-circle-check").classList.add("hidden");
+//document.querySelector("fa-regular fa-circle").classList.remove("hidden");
+//}
 //# sourceMappingURL=script.js.map
